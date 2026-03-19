8a962f6491a1300aa9a10160623afcc097b51c19
Kanishk Suryavanshi <kanishksuryavanshi14@gmail.com>
2026-03-19 23:24:02 +0530

    Calculator_version-02
    added functions
    
    added operator-based choice
    
    added division by zero handling
    
    cleaner structure

---
+++ 02_Calculator.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int quo(int a, int b) {
+    return a / b;
+}
+
+int per(int a, int b) {
+    return a % b;
+}
+
+int main() {
+    cout << "Welcome to calculator\n";
+
+    int x;
+    int y;
+    char z;
+
+    cout << "Enter the first number:\n";
+    cin >> x;
+
+    cout << "Enter the second number:\n";
+    cin >> y;
+
+    cout << "Enter the operator (+, -, *, /, %):\n";
+    cout << "/ is for quotient and % is for remainder.\n";
+    cin >> z;
+
+    if (z == '+') {
+        cout << "The sum is: " << sum(x, y) << "\n";
+    }
+    else if (z == '-') {
+        cout << "The difference is: " << sub(x, y) << "\n";
+    }
+    else if (z == '*') {
+        cout << "The product is: " << mul(x, y) << "\n";
+    }
+    else if (z == '/') {
+        if (y == 0) {
+            cout << "Division by zero is not allowed.\n";
+        } else {
+            cout << "The quotient is: " << quo(x, y) << "\n";
+        }
+    }
+    else if (z == '%') {
+        if (y == 0) {
+            cout << "Division by zero is not allowed.\n";
+        } else {
+            cout << "The remainder is: " << per(x, y) << "\n";
+        }
+    }
+    else {
+        cout << "INVALID OPERATOR\n";
+    }
+
+    return 0;
+}
